4dda3daccdb35ab4b36ad60892a7c66fe7487c44
Yusuf-DataAnalyst <yousifbadr02@gmail.com>
2026-06-02 16:36:19 +0300

    Add files via upload

--- sales_analysis.py
+++ sales_analysis.py
@@ -1,31 +1,35 @@
-#Sales Analysis Project - Accomplished 📉
-product=[]
-price=[]
-Quantity=[]
-totals=[]
-print("******Welcome To Sales Analysis Project - Accomplished 🛒📉 ******")
-while True:
-    name=input("What Is The Name Of Product ? Or Type (q) To Quit  :").lower()
-    if name =="q":
-        if len(totals)>0:
-            total_revenue=sum(totals)
-            max_money=max(totals)
-            the_index=totals.index(max_money)
-            name_of_best=product[the_index]
-            
-            print("""******TOTAL*****""")
-            
-            print(f"Totals:{sum(totals) : .2f} Details :{dict(zip(product,totals))}")
-            print(f"The King Of The Sales is {name_of_best} {max_money:.2f}  ")
-            break
-        else:
-             print("No Data Entered !")
-             break
-    else :
-             product.append(name)
-             price_1=float(input(f"What Is The Price Of {name} $"))
-             price.append(price_1)
-             quantity=int(input(f"What Is The Quantity Of {name}  "))
-             Quantity.append(quantity)
-             result=price_1*quantity
-             totals.append(result)
+sales_data = [
+    {"date": "2026-06-01", "product": "Laptop", "category": "Electronics", "price": 1200, "quantity": 2},
+    {"date": "2026-06-01", "product": "Backpack", "category": "Fashion", "price": 50, "quantity": 5},
+    {"date": "2026-06-02", "product": "Mouse", "category": "Electronics", "price": 25, "quantity": 10},
+    {"date": "2026-06-02", "product": "T-Shirt", "category": "Fashion", "price": 30, "quantity": 3}
+]
+
+total_units = 0
+total_revenue = 0
+category_revenue = {} 
+
+for sales in sales_data:
+
+    current_sale_revenue = sales['price'] * sales['quantity']
+    total_revenue += current_sale_revenue
+    total_units += sales['quantity']
+    category_name = sales['category']
+    
+    if category_name not in category_revenue:
+           category_revenue[category_name] = current_sale_revenue
+    else:
+        category_revenue[category_name] += current_sale_revenue
+
+print('=' * 40)
+print("          Store Sales Report          ")
+print('=' * 40)
+print(f"Total Revenue: ${total_revenue}")
+print(f"Total Units Sold: {total_units} pcs")
+print('-' * 40)
+print("Revenue per Category:")
+
+for category, revenue in category_revenue.items():
+    print(f" - {category}: ${revenue}")
+    
+print('=' * 40)
